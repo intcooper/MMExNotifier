d429a5921c18dd1b2ec1719a5c9199f2f13358ee
Roberto <107645954+intcooper@users.noreply.github.com>
2024-05-24 17:49:43 +0200

    Cleanup infoBadge data template

---
+++ MMExNotifier/MVVM/ExpiringBillToInfoBadgeConverter.cs
@@ -0,0 +1,53 @@
+﻿using MMExNotifier.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Animation;
+using Wpf.Ui.Controls;
+
+namespace MMExNotifier.MVVM
+{
+    internal class ExpiringBillToInfoBadgeConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not ExpiringBill expiringBill)
+            {
+                return null;
+            }
+
+            var infoBadge = new InfoBadge()
+            {
+                Icon = new SymbolIcon(SymbolRegular.Info24, filled: true),
+                Severity = InfoBadgeSeverity.Informational,
+                Style = Application.Current.FindResource("IconInfoBadgeStyle") as Style
+            };
+
+            if (expiringBill.IsExpired)
+            {
+                ((SymbolIcon)infoBadge.Icon).Symbol = SymbolRegular.Warning24;
+                infoBadge.Severity = InfoBadgeSeverity.Critical;
+                return infoBadge;
+            }
+
+            if (expiringBill.DaysToNextOccurrence <= 2)
+            {
+                ((SymbolIcon)infoBadge.Icon).Symbol = SymbolRegular.Warning24;
+                infoBadge.Severity = InfoBadgeSeverity.Caution;
+                return infoBadge;
+            }
+
+            return infoBadge;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
